7597b78b85792ddefcc971f28fe70f0e1777f8c1
pranavtailor <68711844+pranavtailor@users.noreply.github.com>
2021-04-29 14:17:54 -0400

    Add files via upload

--- ls.py
+++ ls.py
@@ -4,30 +4,48 @@ import argparse
 
 parser = argparse.ArgumentParser(description="""This is a very basic client program""")
 parser.add_argument('port', type=int, help='This is the port ls will listen and connect to the client', action='store')
+parser.add_argument('ts1host', type=str, help='This is the host of ts1', action='store')
+parser.add_argument('ts1port', type=int, help='This is the port of ts1', action='store')
+parser.add_argument('ts2host', type=str, help='This is the host of ts2', action='store')
+parser.add_argument('ts2port', type=int, help='This is the port of ts2', action='store')
 args = parser.parse_args(argv[1:])
 
-#host = socket.gethostname()
-print(f'[S]: Host is - {host}')
+# host = socket.gethostname()
+# print(f'[S]: Host is - {host}')
 
+# create server socket to communicate with client
 try:
     ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print('[S]: Server socket created')
-
+    print("[S]: Server socket created")
 except socket.error as err:
-    print(f"[S]: Couldn't create socket due to {err}")
+    print("[S]: Couldn't create socket due to {}".format(err))
     exit()
-SERVER = ('', args.port)
-ss.bind((SERVER))
+
+# choose a port for the server
+server_addr = (socket.gethostname(), args.port)
+ss.bind(server_addr)
 ss.listen(1)
+
+# print server information
+host = socket.gethostname()
+print("[S]: The host is {}".format(host))
+localhost_ip = (socket.gethostbyname(host))
+print("[S]: Server IP: {}".format(localhost_ip))
+
+# accept a client
+csockid, addr = ss.accept()
+print("[S]: Got a connection, client is at {}".format(addr))
 
 connection, address = ss.accept()
 
-with connection:
-    while True:
-        data = connection.recv(512)
-        data = data.decode('utf-8')
-        #send query to hash function
-        connection.sendall(data.encode('utf=8'))
+while True:
+    clientData = csockid.recv(512)
+    if not clientData:
+        break
+    clientData = clientData.decode('utf-8')
+    # send query to hash function
+    connection.sendall(clientData.encode('utf=8'))
 
+# Close the server socket
 ss.close()
 exit()
--- ts2.py
+++ ts2.py
@@ -1,9 +1,11 @@
+import binascii
 import socket
 import argparse
 from sys import argv
 
+# First we use the argparse package to parse the arguments
 parser = argparse.ArgumentParser(description="""This is a very basic client program""")
-parser.add_argument('port', type=int, help='This is the port to ts1 will listen for.', action='store')
+parser.add_argument('port', type=int, help='This is the port that ts2 will listen for.', action='store')
 args = parser.parse_args(argv[1:])
 
 class Hostname():
@@ -17,7 +19,8 @@ def add_host(host, ip):
     host_name = Hostname(host, ip)
     table.append(host_name)
 
-
+# checks to see if hostname is in the DNS Table
+# if not, send dns request to Google
 def lookup(hostname):
     found = False
     for i in table:
@@ -33,29 +36,103 @@ def lookup(hostname):
 
 try:
     ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = socket.gethostname()
-    localhost_ip = (socket.gethostbyname(host))
-    print('[S]: Server socket created')
-    print('[S]: The host is {}'.format(host))
-    print('[S]: Server IP address is {}'.format(localhost_ip))
+    print("[S]: Socket created")
 except socket.error as err:
-    print(f"[S]: Couldn't create socket due to {err}")
+    print("[S]: Couldn't create socket due to {}".format(err))
     exit()
 
-# choose a port
-SERVER = ('', args.port)
-ss.bind(SERVER)
+# choose a port for the server
+server_addr = (socket.gethostname(), args.port)
+ss.bind(server_addr)
 ss.listen(1)
 
+# print server information
+host = socket.gethostname()
+print("[S]: The host is {}".format(host))
+localhost_ip = (socket.gethostbyname(host))
+print("[S]: Server IP: {}".format(localhost_ip))
+
 # accept a client
-connection, address = ss.accept()
+csockid, addr = ss.accept()
+print("[S]: Got a connection, client is at {}".format(addr))
 
-with connection:
-    while True:
-        data = connection.recv(512)
-        data = data.decode('utf-8')
-        data = lookup(data)
-        connection.sendall(data.encode('utf=8'))
+# starting here is the code from project 3
+# This function sends a message to the UDP server
+# Function taken from resource: "https://routley.io/posts/hand-writing-dns-messages/"
+def send_udp_message(message, address, port):
+    message = message.replace(" ", "").replace("\n", "")
+    server_address = (address, port)
 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        sock.sendto(binascii.unhexlify(message), server_address)
+        data, _ = sock.recvfrom(4096)
+    finally:
+        sock.close()
+    return binascii.hexlify(data).decode("utf-8")
+
+
+# This function returns a pretty version of a hex string
+
+# This function concatenates each element in the list together
+def concatenateList(list):
+    result = ""
+    for element in list:
+        result += str(element)
+        result += ' '
+    return result
+
+
+# This function makes the final hexadecimal DNS query
+def domainToHex(domainName):
+    firstPart = "AA AA 01 00 00 01 00 00 00 00 00 00 "
+    lastPart = "00 01 00 01"
+
+    domainName_list = domainName.split(".")
+    stringcount = len(domainName_list)
+    ListofSizesOfParts = []
+
+    for i in range(stringcount):
+        if len(domainName_list[i]) < 10:
+            ListofSizesOfParts.append("%02d" % len(domainName_list[i]))
+            toHex(domainName_list[i], ListofSizesOfParts)
+        elif len(domainName_list[i]) >= 10:
+            ListofSizesOfParts.append(len(domainName_list[i]))
+            toHex(domainName_list[i], ListofSizesOfParts)
+    ListofSizesOfParts.append('00')
+
+    finalResult = firstPart + concatenateList(ListofSizesOfParts) + lastPart
+    return finalResult
+
+
+while True:
+    clientData = csockid.recv(512)
+    if not clientData:
+        break
+    clientData = clientData.decode('utf-8')
+
+    DNSquery = domainToHex(clientData)
+
+    response = send_udp_message(DNSquery, "8.8.8.8", 53)
+    finResponse = format_hex(response)
+
+    finResponseList = finResponse.split()
+    res = finResponseList[-4:]
+
+    final = []
+    for i in range(len(res)):
+        final.append(int(res[i], 16))
+
+    finalIP = ""
+    for element in final:
+        finalIP += str(element)
+        finalIP += '.'
+    finalIP = finalIP[:-1]
+
+    csockid.send(finalIP.encode('utf-8'))
+
+# Close the server socket
 ss.close()
 exit()
+
+
